580884912a0c34019d97a3d73168d0b97170e6d8
Benny Neugebauer <mail@bennycode.com>
2024-06-05 11:57:03 +0200

    Initial commit

---
+++ src/sayHello.test.ts
@@ -0,0 +1,8 @@
+import {sayHello} from './sayHello.js';
+
+describe('sayHello', () => {
+  it('prints to the console', () => {
+    const text = `${sayHello()} ${process.env.NODE_ENV}`;
+    expect(text).toEqual(`I am Command Line Interface test`);
+  });
+});
