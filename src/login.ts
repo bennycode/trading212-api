7c428fe18fb7ed7466f441d49e45b842d2db1b30
Benny Neugebauer <mail@bennycode.com>
2024-06-28 11:42:19 +0300

    login works

--- src/login.ts
+++ src/login.ts
@@ -1,17 +1,25 @@
+import 'dotenv-defaults/config.js';
 import {chromium} from 'playwright';
 
 const browser = await chromium.launch({headless: false});
 const page = await browser.newPage();
 const baseUrl = 'https://app.trading212.com/';
 await page.goto(baseUrl, {waitUntil: 'networkidle'});
-const emailXPath = '//input[@type="email"]';
-const passwordXPath = '//input[@type="password"]';
+
+const emailSelector = '//input[@type="email"]';
+await page.waitForSelector(emailSelector);
+await page.fill(emailSelector, `${process.env.TRADING212_EMAIL}`);
+
+const passwordSelector = '//input[@type="password"]';
+await page.waitForSelector(passwordSelector);
+await page.fill(passwordSelector, `${process.env.TRADING212_PASSWORD}`);
+
 const submitButtonXPath = '//div[text()="Log in"]';
-await page.waitForSelector(emailXPath);
-await page.fill(emailXPath, 'example@example.com');
-await page.waitForSelector(passwordXPath);
-await page.fill(passwordXPath, '123456');
 await page.waitForSelector(submitButtonXPath);
 await page.click(submitButtonXPath);
+
+const globalMarketsSelector = '[data-testid="Global markets"]';
+await page.waitForSelector(globalMarketsSelector);
+
 await page.screenshot({path: 'example.png'});
 await browser.close();
